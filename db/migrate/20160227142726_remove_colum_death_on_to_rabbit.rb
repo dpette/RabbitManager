668c9e2db7ecef83f1fcf018725337086fcc9330
pette <pettenon.daniel@gmail.com>
2016-02-27 19:20:13 +0100

    remove a thing

--- db/migrate/20160227142726_remove_colum_death_on_to_rabbit.rb
+++ db/migrate/20160227142726_remove_colum_death_on_to_rabbit.rb
@@ -1,6 +1,6 @@
 class RemoveColumDeathOnToRabbit < ActiveRecord::Migration
   def change
     remove_column :rabbits, :death_on, :date
-    remove_column :rabbits, :cage_id, :integer
+    # remove_column :rabbits, :cage_id, :integer
   end
 end
